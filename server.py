8381bae47377672d39a8f5c1d8a0ea65b5e58d46
Christian Navolskyi <c.navolskyi@gmail.com>
2019-03-26 10:47:52 +0100

    Changed to allow adding custom message to emergency.

--- server.py
+++ server.py
@@ -11,15 +11,15 @@ def welcome():
     return "Welcome on our notification platform. This is where lives are saved."
 
 
-@route("/emergency/<location>/")
-@route("/emergency/<location>")
+@route("/emergency/<message>/")
+@route("/emergency/<message>")
 @route("/emergency/")
 @route("/emergency")
-def test(location=None):
+def test(message=None):
     logging.info("Emergency was triggered.")
     emergency_text = "There is an emergency!"
-    if location:
-        emergency_text = "There is an emergency at " + str(location) + "!"
+    if message:
+        emergency_text = str(message)
     sender.send_message_to_all_chats(emergency_text)
     return emergency_text
 
